36269d8a1c01f060d1f8cb042f8002d591c663a0
sooahkim <saligo96@gmail.com>
2019-10-30 21:25:02 +0900

    과제

--- Restframework/rest/models.py
+++ Restframework/rest/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Essay(models.Model):
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,default=1,on_delete=models.CASCADE)
+    title=models.CharField(max_length=30)
+    body=models.TextField(max_length=200)
+
+class Album(models.Model):
+    author= models.ForeignKey(settings.AUTH_USER_MODEL,default=1,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to="images")
+    desc=models.CharField(max_length=100)
+
+class Files(models.Model):
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,default=1,on_delete=models.CASCADE)
+    myfile=models.FileField(blank=False,null=False,upload_to="")
+    desc=models.CharField(max_length=100)
--- Restframework/rest/views.py
+++ Restframework/rest/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Essay,Album,Files
+from .serializers import EssaySerializer,AlbumSerializer,FileSerializer
+from rest_framework.filters import SearchFilter
+from rest_framework.parsers import MultiPartParser,FormParser
 # Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset=Essay.objects.all()
+    serializer_class=EssaySerializer
+
+    filter_backends=[SearchFilter]
+    search_fields=('title','body')
+
+    def perform_create(self,serializers):
+        serializers.save(author=self.request.user)
+    
+    #현재 request보낸 유저
+    #self.request.user
+
+    def get_queryset(self):
+        qs=super().get_queryset()
+
+        if self.request.user.is_authenticated:
+            qs=qs.filter(author=self.request.user)
+        else:
+            qs=qs.none()
+
+        return qs
+class ImgViewSet(viewsets.ModelViewSet):
+    queryset=Album.objects.all()
+    serializer_class=AlbumSerializer
+
+from rest_framework.response import Response
+from rest_framework import status
+class FileViewSet(viewsets.ModelViewSet):
+    queryset=Files.objects.all()
+    serializer_class=FileSerializer
+
+    parser_classes=(MultiPartParser,FormParser)
+
+    def post(self,request,*args,**kwargs):
+        serializer=FileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=HTTP_201_CREATED)
+        else:
+            return Response(serializer.error,status=HTTP_404_BAD_REQUEST)
+
+
